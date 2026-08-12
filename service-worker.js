e2bdce2e5f93820e43dccffb004b806db55ba1f1
Konstantin Vasyukov <35726198+nl4rvr@users.noreply.github.com>
2026-08-12 16:02:42 +0300

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'victoria-calendar-v2';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png',
+    '/icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+            .catch(() => new Response('Офлайн режим', { status: 503 }))
+    );
+});
